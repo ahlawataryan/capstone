ee61e7fa78d28bccc1a31a0880c0d254fcbde506
Rory-Michael Christlieb <73552535+Rorr4@users.noreply.github.com>
2026-02-12 20:11:58 -0600

    Create db.js

---
+++ db.js
@@ -0,0 +1,42 @@
+// db.js
+const sqlite3 = require("sqlite3").verbose();
+const fs = require("fs");
+const filepath = "./capstone.db";
+
+function createDbConnection() {
+  if (fs.existsSync(filepath)) {
+    console.log("Connecting to existing database");
+    return new sqlite3.Database(filepath);
+  } else {
+    console.log("Creating new database and connecting");
+    const db = new sqlite3.Database(filepath, (error) => {
+      if (error) {
+        return console.error(error.message);
+      }
+      createTable(db);
+    });
+    return db;
+  }
+}
+
+function createTable(db) {
+  const createTableSql = `
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TINYTEXT UNIQUE NOT NULL,
+      email TINYTEXT UNIQUE NOT NULL,
+      password TINYTEXT NOT NULL,
+      accountType ENUM(Admin, Client, Student)
+    );
+  `;
+
+  db.run(createTableSql, (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log("Table created successfully");
+  });
+}
+
+module.exports = createDbConnection();
+
